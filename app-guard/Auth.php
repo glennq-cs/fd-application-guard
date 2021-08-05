e0002f915621610a1e8782f6918c6e471bd645b5
glennq <glennq@cloudstaff.com>
2021-08-05 15:37:34 +0800

    sprint25_FD-19 #comment: adding namespace

---
+++ app-guard/Auth.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace  Glennq\Cs\Fd\Application\Guard\App\Guard;
+
+class Auth
+{
+    public function __construct()
+    {
+        dd('test');
+    }
+}
+
+// Interface Auth
+// {
+//     public function check();
+// }
